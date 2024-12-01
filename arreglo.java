c5c7e5a3744e5a8b111f8e7d42442d4135b8d498
EstherAvalos <teteavalosgarcia2@gmail.com>
2024-11-30 18:11:03 -0600

    Create arreglo.java

---
+++ arreglo.java
@@ -0,0 +1,14 @@
+public class arreglo {
+
+    public static void main(String[] args) {
+        int[] numero =new int[3];
+        numero[0]=7;
+        numero[1]=10;
+        numero[2]=13;
+
+        System.out.println(numero[0]);// se va a imprimir el valor que tiene el arrgelo en que se le asigni al [0]
+        for(int i=0;i<3;i++){
+            System.out.println(numero[i]);//se van a imprimir todods los valores que se le asignaron al arregrlo
+        }
+    }
+}
